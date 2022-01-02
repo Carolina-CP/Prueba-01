6d63a30679280c97c462e4e6f00c64906cc56252
carola <carolacollao@gmail.com>
2022-01-02 11:35:29 -0300

    cambio tooltips y funciones js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,39 @@
-$(function () {
-   
-    $('[data-toggle="tooltip"]').tooltip()
+
+$(document).ready(function () {
+
+  $(function () {
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+  
+   $("#submit").click(function (e) { 
+    e.preventDefault();
+  console.log("miauuuu!");
 
 
-  })
+    var nombre= $("#userName").val();
+    var asunto= $("#userSubject").val();
+    var mensaje= $("#userMessage").val();
 
+  console.log({nombre, asunto, mensaje});
+
+   $("#user-name").text(nombre);
+   $("#user-subject").text(asunto);
+   $("#user-message").text(mensaje);
+
+  $("#confirm-submit").click(function (e) { 
+    e.preventDefault();
+    console.log("miauuuu")
+    
+    $("#contacto")[0].reset();
+    $(".modal").modal("toggle");
+
+  });
+    
+  });
+    
+  });
+
+
+
+
+  
